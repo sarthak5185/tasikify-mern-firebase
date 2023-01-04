699ae9e294fe3339a4a667f556e2dacd48fb8993
Sarthak Jain <jainsar123@gmail.com>
2023-01-04 21:49:59 +0530

    authen com

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const cors=require("cors");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin:["http://localhost:3000","https://mern-task-app.onrender.com"]
+    origin:["http://localhost:3000","https://taskify-mern.onrender.com"]
 }));
 // app.use(cors());
 
